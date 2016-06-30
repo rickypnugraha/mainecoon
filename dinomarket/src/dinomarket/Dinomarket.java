2580a463398d0909548e59c13525f2e96ff50276
rickypnugraha <rickypnugraha@gmail.com>
2016-06-30 20:07:47 +0700

    add 2 scenarios
    add scenarios

--- dinomarket/src/dinomarket/Dinomarket.java
+++ dinomarket/src/dinomarket/Dinomarket.java
@@ -1,8 +1,11 @@
 package dinomarket;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,16 +17,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Dinomarket {    
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.setProperty("webdriver.chrome.driver", "D://SeleniumProject/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         
-    //Scenario 1
+       
+    //Scenario 1 - as a user I want to login using my registered account!
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        System.out.println("SCENARIO 1");
         System.out.println("1. Go to Dinomarket site");
         driver.navigate().to("http://www.dinomarket.com");
+        FileUtils.copyFile(scrFile, new File("C:\\Users\\HP\\Pictures\\web\\web.png"));
         
+        // Now you can do whatever you need to do with it, for example copy somewhere
         System.out.println("2. Select LOGIN button");
         driver.findElement(By.xpath("//img[contains(@src,'http://media.dinomarket.com/system/themes/th1/navup/bt_login_n_h5.png')]")).click();
         
@@ -32,15 +40,47 @@ public class Dinomarket {
         
         System.out.println("4. Enter username/ e-mail");
         //driver.findElement(By.name("loginname")).clear();
-        driver.findElement(By.name("loginname")).sendKeys("kunyuk@gmail.com");
-        
+        driver.findElement(By.name("loginname")).sendKeys("rickyselenium");
+        Thread.sleep(2000);
+       
         System.out.println("5. Enter Password");     
         driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys("kunyuk");
-    
+        driver.findElement(By.name("password")).sendKeys("ja63rh");
+        Thread.sleep(2000);
+        
         System.out.println("6. Select LOGIN button");    
         driver.findElement(By.xpath("/html/body/div[4]/div/div/table/tbody/tr[4]/td[2]/form/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[4]/td[2]/table/tbody/tr[4]/td[3]/input")).click();
+        System.out.println("7. If login is success, then it will redirect to My Profile page"); 
+               
+        
+    //Scenario 2 - as a user I want to add product to the cart
+        System.out.println("SCENARIO 2");
+        System.out.println("1. select Dinomarket icon");
+        driver.findElement(By.xpath("/html/body/table[1]/tbody/tr[1]/td/center/table/tbody/tr/td/center/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a/img")).click();
+        
+        System.out.println("2. select smartphone menu");        
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a")).click();
+        Thread.sleep(2000);
+        
+        System.out.println("3. select galaxy s7 device");
+        driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td/center/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[3]/table[3]/tbody/tr[5]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/a/table/tbody/tr[3]/td/table/tbody/tr[5]/td/b")).click();
+        Thread.sleep(2000);
+        
+        System.out.println("4. select Beli Sekarang button");
+        driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td/table/tbody/tr/td/center/table/tbody/tr[1]/td/table/tbody/tr[3]/td/table/tbody/tr[1]/td[4]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td[2]/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[3]/table/tbody/tr[11]/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[13]/td[1]/img")).click();
+        Thread.sleep(2000);
+        
+        System.out.println("5. close cart pop-up");
+        driver.findElement(By.xpath("/html/body/div[7]")).click();
+        Thread.sleep(2000);
        
-        System.out.println("7. If login is success, then it will redirect to My Profile page");     
+        
+        //Scenario 3 - as a user i want to ask the customer service using LIVE Chat menu
+        System.out.println("SCENARIO 3");
+        System.out.println("1. select LAYANAN LIVE CHAT menu");
+        driver.findElement(By.xpath("/html/body/table/tbody/tr[1]/td/a/table/tbody/tr/td/center/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[5]/a/img[2]")).click();
+        Thread.sleep(10000);
+       
+        driver.quit();
     }
 }
